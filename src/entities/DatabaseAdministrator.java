1ffdaca610e7a563e36cc99ee1d7c75a387cdf1b
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-14 21:05:37 -0300

    Payroll Taxes

--- src/entities/DatabaseAdministrator.java
+++ src/entities/DatabaseAdministrator.java
@@ -6,9 +6,9 @@ public class DatabaseAdministrator implements JobPosition {
 
 	public double total(double salary) {
 		if (salary > 3000) {
-			return salary + (0.15 * salary);
+			return salary - (0.15 * salary);
 		} else {
-			return salary + (0.20 * salary);
+			return salary - (0.20 * salary);
 		}
 	}	
 }
--- src/entities/Developer.java
+++ src/entities/Developer.java
@@ -6,9 +6,9 @@ public class Developer implements JobPosition {
 	
 	public double total(double salary) {
 		if (salary > 3000) {
-			return salary + (0.1 * salary);
+			return salary - (0.1 * salary);
 		} else {
-			return  salary + (0.2 * salary);
+			return  salary - (0.2 * salary);
 		}
 	}
 }
